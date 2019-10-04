d978ab358b98b5a055bdd81904e282b9d4df9dd7
Stephen Anderson <ruralcoder@gmail.com>
2019-10-04 15:57:37 -0700

    Applied the new fields to the sample data

--- source/Models.swift
+++ source/Models.swift
@@ -67,11 +67,13 @@ enum Avocado: String {
 struct CompletedOrder: Identifiable {
     var id: UUID
     var summary: String
-    var purchaseDate: String
-    var quantity: Int = 1
+    
+    
     var bread: Bread = .sourdough
     var avocado: Avocado = .spread
     var spread: Spread = .none
+    var purchaseDate: String
+    var quantity: Int = 1
     var toppings: Array<Toppings>
     
     var notes: String = "Dense nutty flavor with aromas of dark currants, fragrant tobacco, pencil shavings and blossom flowers. Full-bodied with racy acidity.\n\nBread had been chilled at 4˚C (slightly too cold) before being toasted for 216 seconds at 154˚C. Laid upon a Vosges oak cutting board before being spread with 16 grams of organic free range almond butter."
@@ -80,15 +82,15 @@ struct CompletedOrder: Identifiable {
 
 
 var sampleOrders = [
-    CompletedOrder(id: UUID(), summary: "Rye with Almond Butter", purchaseDate: "5/30/19 8:47PM", quantity: 1, toppings: [.salt, .redPepperFlakes]),
-    CompletedOrder(id: UUID(), summary: "Multi-Grain with Humous", purchaseDate: "5/29/19 5:00PM", quantity: 2, toppings: [.redPepperFlakes]),
-    CompletedOrder(id: UUID(), summary: "Multi-Grain toast", purchaseDate: "5/28/19 1:13PM", quantity: 1, toppings: [.salt, .chives]),
-    CompletedOrder(id: UUID(), summary: "Sourdough with Chutney", purchaseDate: "5/27/19 9:47PM", quantity: 1, toppings: [.salt, .redPepperFlakes]),
-    CompletedOrder(id: UUID(), summary: "Rye with Peanut Butter", purchaseDate: "5/26/19 5:40PM", quantity: 1, toppings: [.salt, .redPepperFlakes, .eggs]),
-    CompletedOrder(id: UUID(), summary: "Wheat with Tapenade", purchaseDate: "5/25/19 5:40 PM",  quantity: 1, toppings: [.pickledOnion, .watermelonRadish, .chives]),
-    CompletedOrder(id: UUID(), summary: "Sourdough with Vegemite", purchaseDate: "5/23/19 5:40 PM", quantity: 1, toppings: [.salt]),
-    CompletedOrder(id: UUID(), summary: "Wheat with Féroce", purchaseDate: "5/22/19 5:40 PM", quantity: 1, toppings: [.salt, .redPepperFlakes, .eggs]),
-    CompletedOrder(id: UUID(), summary: "Rye with Honey", purchaseDate: "5/21/19 2:33 PM", quantity: 1, toppings: []),
-    CompletedOrder(id: UUID(), summary: "Multi-Grain Toast", purchaseDate: "5/20/19 10:47 AM", quantity: 1, toppings: [.salt, .eggs]),
-    CompletedOrder(id: UUID(), summary: "Sourdough with Chutney", purchaseDate: "5/19/19 7:00 AM", quantity: 1, toppings: []),
+    CompletedOrder(id: UUID(), summary: "Rye with Almond Butter", bread:.rye, spread: .almondbutter, purchaseDate: "5/30/19 8:47PM", quantity: 1, toppings: [.salt, .redPepperFlakes]),
+    CompletedOrder(id: UUID(), summary: "Multi-Grain with Hummus", bread:.multigrain, spread: .hummus, purchaseDate: "5/29/19 5:00PM", quantity: 2, toppings: [.redPepperFlakes]),
+    CompletedOrder(id: UUID(), summary: "Multi-Grain toast", bread:.multigrain, purchaseDate: "5/28/19 1:13PM", quantity: 1, toppings: [.salt, .chives]),
+    CompletedOrder(id: UUID(), summary: "Sourdough with Chutney", bread:.sourdough, spread: .chutney, purchaseDate: "5/27/19 9:47PM", quantity: 1, toppings: [.salt, .redPepperFlakes]),
+    CompletedOrder(id: UUID(), summary: "Rye with Peanut Butter", bread:.rye, spread: .peanutbutter, purchaseDate: "5/26/19 5:40PM", quantity: 1, toppings: [.salt, .redPepperFlakes, .eggs]),
+    CompletedOrder(id: UUID(), summary: "Wheat with Tapenade", bread:.wheat, spread: .tapenade, purchaseDate: "5/25/19 5:40 PM",  quantity: 1, toppings: [.pickledOnion, .watermelonRadish, .chives]),
+    CompletedOrder(id: UUID(), summary: "Sourdough with Vegemite", bread:.sourdough, spread: .vegemite, purchaseDate: "5/23/19 5:40 PM", quantity: 1, toppings: [.salt]),
+    CompletedOrder(id: UUID(), summary: "Wheat with Féroce", bread:.wheat, spread: .feroce, purchaseDate: "5/22/19 5:40 PM", quantity: 1, toppings: [.salt, .redPepperFlakes, .eggs]),
+    CompletedOrder(id: UUID(), summary: "Rye with Honey", bread:.rye, spread: .honey, purchaseDate: "5/21/19 2:33 PM", quantity: 1, toppings: []),
+    CompletedOrder(id: UUID(), summary: "Multi-Grain Toast", bread:.multigrain, purchaseDate: "5/20/19 10:47 AM", quantity: 1, toppings: [.salt, .eggs]),
+    CompletedOrder(id: UUID(), summary: "Sourdough with Chutney", bread:.sourdough, spread: .chutney, purchaseDate: "5/19/19 7:00 AM", quantity: 1, toppings: []),
 ]
